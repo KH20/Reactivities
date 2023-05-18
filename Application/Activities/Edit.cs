d20e8cf6dc9d1c60ce46357cb5fb15ee725b07e2
KH20 <kurt.hamer.90@gmail.com>
2023-05-18 23:29:30 +0100

    Added automapper for the edit function

--- Application/Activities/Edit.cs
+++ Application/Activities/Edit.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Domain;
 using MediatR;
 using Persistence;
@@ -10,25 +11,26 @@ namespace Application.Activities
 {
     public class Edit
     {
-        public class Command : IRequest{
+        public class Command : IRequest
+        {
             public Activity Activity { get; set; }
         }
 
         public class Handler : IRequestHandler<Command>
         {
             private readonly DataContext context;
-            public Handler(DataContext context)
+            private readonly IMapper mapper;
+            public Handler(DataContext context, IMapper mapper)
             {
+                this.mapper = mapper;
                 this.context = context;
             }
 
             public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
             {
                 var activity = await this.context.Activities.FindAsync(request.Activity.Id);
-                
-                activity.Category = request.Activity.Category ?? activity.Category; 
-                activity.Title = request.Activity.Title ?? activity.Title;
-                activity.Date = request.Activity.Date;
+
+                this.mapper.Map(request.Activity, activity);
 
                 await this.context.SaveChangesAsync();
 
