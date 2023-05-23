d10aa80bc0ec3876e5df38b6f1c4b6d546c80302
KH20 <kurt.hamer.90@gmail.com>
2023-05-23 13:09:17 +0100

    Revert "A cancellation token example, but not used in the end product - Wasn't needed in this branch"
    This reverts commit d2f021a995d2237811321ef1379f7e574d880f17.

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,15 +1,13 @@
 using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
 using Persistence;
 
 namespace Application.Activities
 {
     public class List
     {
-        public class Query : IRequest<List<Activity>>
-        {
+        public class Query : IRequest<List<Activity>>{
 
         }
 
@@ -17,30 +15,13 @@ namespace Application.Activities
         {
             private readonly DataContext context;
 
-            private readonly ILogger<List> logger;
-
-            public Handler(DataContext context, ILogger<List> logger)
-            {
-                this.logger = logger;
+            public Handler(DataContext context){
                 this.context = context;
 
             }
 
             public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
             {
-                try
-                {
-                    for (var i = 0; i < 10; i++)
-                    {
-                        cancellationToken.ThrowIfCancellationRequested();
-                        await Task.Delay(1000, cancellationToken);
-                        this.logger.LogInformation($"Task {i} has completed");
-                    }
-                }
-                catch (System.Exception)
-                {
-                    this.logger.LogInformation("Task was cancelled");
-                }
                 return await this.context.Activities.ToListAsync();
             }
         }
